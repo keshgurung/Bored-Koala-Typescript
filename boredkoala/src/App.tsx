58b7d9f7057a3a4a84cef0c954879dcc5dd1c4c3
keshgurung <k_grg@hotmail.com>
2022-03-23 22:04:01 +0000

    refs and cross components in ts

--- boredkoala/src/App.tsx
+++ boredkoala/src/App.tsx
@@ -4,9 +4,14 @@ import MovieList from './components/MovieList';
 import NewTodo from './components/NewTodo';
 const App: React.FC = () => {
   const todos = [{'id': 1, "name": 'call mom'}]
+
+  // we add a fn to point this to NewTodo component, to be called there.
+  const todoAddHandler = (test:string) =>{
+
+  }
   return <div className='app'>
     <MovieList items ={todos} />
-    <NewTodo />
+    <NewTodo onAddTodo={todoAddHandler} />
   </div>
 
 }
--- boredkoala/src/components/NewTodo.tsx
+++ boredkoala/src/components/NewTodo.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React ,{ useRef} from 'react'
 
-const NewTodo: React.FC = () => {
+interface newTodoProps {
+ onAddTodo: (todoText: string) => void
+}
+
+const NewTodo: React.FC<newTodoProps> = props => {
+  const textInputRef = useRef<HTMLInputElement>(null)
   const todoSubmitHandler = (e: React.FormEvent) =>{
   e.preventDefault()
-  }
+  const enteredText = textInputRef.current!.value 
+  //current is used with useRef and ! says it is okay to ts. 
+  props.onAddTodo(enteredText)
+}
 
   return (
     <form onSubmit={todoSubmitHandler} >
     <div>
       <label htmlFor='todo-text'>Todo Text</label>
-      <input type="text" id='todo-text' />
+      <input type="text" id='todo-text' ref={textInputRef} />
     </div>
     <button type='submit'>ADD TODO</button>
     </form>
